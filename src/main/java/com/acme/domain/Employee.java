e9ef71cd68df7d0c3269880b69e06c3fe1440024
Hara Totapally <htotapally@yahoo.com>
2018-04-29 16:39:29 -0400

    Initial Commit

---
+++ src/main/java/com/acme/domain/Employee.java
@@ -0,0 +1,8 @@
+package com.acme.domain;
+
+public class Employee {
+
+	public Employee() {
+		System.out.println("Instantiating Employee!");
+	}
+}
---
+++ src/test/java/com/acme/domain/EmployeeTest.java
@@ -0,0 +1,14 @@
+package com.acme.domain;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+class EmployeeTest {
+
+	@Test
+	void testEmployee() {
+		fail("Not yet implemented");
+	}
+
+}
