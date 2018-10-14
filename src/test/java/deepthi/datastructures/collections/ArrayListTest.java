163d439a2decb4bb3383d3117a35eaaac6e8a707
DeepthiVaranasi <varanasideepthi@gmail.com>
2018-10-14 17:06:45 +0530

    Removed commented out code and blankspaces

--- src/test/java/deepthi/datastructures/collections/ArrayListTest.java
+++ src/test/java/deepthi/datastructures/collections/ArrayListTest.java
@@ -1,5 +1,7 @@
 package deepthi.datastructures.collections;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class ArrayListTest {
@@ -18,46 +20,15 @@ public class ArrayListTest {
 	
 	@Test
 	public void testAddElement() {
-		
-		
-		ArrayList arrayList = new ArrayList();
-		
-		/*
-		arrayList.addElement(11);
-		arrayList.addElement(12);
-		arrayList.addElement(13);
-		arrayList.addElement(14);
-		arrayList.addElement(15);
-		
-		int size = arrayList.getSize();
-		System.out.println(size);
-		for (int i = 0; i < size; i++) {
-			int element = arrayList.getElement(i);
-			System.out.println(element);
-		}
-		*/
-		
-		// This is a brute way of testing arrays
-		// Better way
-		
+		ArrayList arrayList = new ArrayList();	
 		int[] myElements = {11, 12, 13, 14, 15};
-		
-		// I can add all these element in a loop
-		
 		for(int i = 0; i < myElements.length; i++) {
 			arrayList.addElement(myElements[i]);
-		}
-		
-		// First ensure that the size of the ArrayList		
+		}		
 		assertEquals(arrayList.getSize(), 5);
-		
-		// Now I can easily test my elements in the arraylist
-		// There is a one to one mapping between myElements, and the elements in the ArrayList
 		for(int i = 0; i < myElements.length; i++) {
 			assertEquals(myElements[i], arrayList.getElement(i));
 		}
-		
-		
 	}
 	
 	@Test
