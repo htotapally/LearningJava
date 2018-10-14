0bd9dd59c26d0bc67b21dd360d92920e7328cb52
htotapally <Sharma@IF-SWS-HHXJ5L2.ad.ufl.edu>
2018-10-14 07:29:54 -0400

    Added code for better testing of arrays

--- src/test/java/deepthi/datastructures/collections/ArrayListTest.java
+++ src/test/java/deepthi/datastructures/collections/ArrayListTest.java
@@ -18,7 +18,11 @@ public class ArrayListTest {
 	
 	@Test
 	public void testAddElement() {
+		
+		
 		ArrayList arrayList = new ArrayList();
+		
+		/*
 		arrayList.addElement(11);
 		arrayList.addElement(12);
 		arrayList.addElement(13);
@@ -31,6 +35,29 @@ public class ArrayListTest {
 			int element = arrayList.getElement(i);
 			System.out.println(element);
 		}
+		*/
+		
+		// This is a brute way of testing arrays
+		// Better way
+		
+		int[] myElements = {11, 12, 13, 14, 15};
+		
+		// I can add all these element in a loop
+		
+		for(int i = 0; i < myElements.length; i++) {
+			arrayList.addElement(myElements[i]);
+		}
+		
+		// First ensure that the size of the ArrayList		
+		assertEquals(arrayList.getSize(), 5);
+		
+		// Now I can easily test my elements in the arraylist
+		// There is a one to one mapping between myElements, and the elements in the ArrayList
+		for(int i = 0; i < myElements.length; i++) {
+			assertEquals(myElements[i], arrayList.getElement(i));
+		}
+		
+		
 	}
 	
 	@Test
