91d2b73647096452832246200e6f1c3d258d80d8
DeepthiVaranasi <varanasideepthi@gmail.com>
2019-03-03 16:32:22 +0530

    Initial commit

---
+++ src/main/java/com/acme/domain/IfDemo.java
@@ -0,0 +1,15 @@
+package com.acme.domain;
+
+public class IfDemo {
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 6;
+		if(a == b) {
+			System.out.println("A is equal to b");
+		} else {
+			System.out.println("A not equal to b");
+		}
+		
+	}
+
+}
