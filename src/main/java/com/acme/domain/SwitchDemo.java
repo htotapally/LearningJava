d06e31f983e5aa80701c5962a90fda133ed06dc8
DeepthiVaranasi <varanasideepthi@gmail.com>
2019-03-03 16:57:17 +0530

    Initial Commit

---
+++ src/main/java/com/acme/domain/SwitchDemo.java
@@ -0,0 +1,25 @@
+package com.acme.domain;
+
+public class SwitchDemo {
+	public static void main(String[] args) {
+		char i = 'a';
+		switch (i) {
+		case 'a':
+			System.out.println("You have selected a");
+			break;
+		case 'b':
+			System.out.println("You have selected b");
+			break;
+		case 'c':
+			System.out.println("You have selected c");
+			break;
+		case 'd':
+			System.out.println("You have selected d");
+			break;
+		case 'e':
+			System.out.println("You have selected e");
+			break;
+		}
+	}
+
+}
